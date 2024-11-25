8a18556e4d0ef8ef06a77cada8f68f1a7ff995f4
MaxFr300 <a24310119@ceti.mx>
2024-11-24 22:49:30 -0600

    Actualización
    Implemente la interfaz a pruebas.

--- main.c
+++ main.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <math.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hola, Mundo\n");
+	
+	int opcion;
+	
+	printf("Menu de seleccion de figura: \n");
+	
+		printf("1. Circulo\n");
+		printf("2. Triangulo\n");
+		printf("3. Cuadrado\n");
+		Printf("4. Rectangulo\n");
+		printf("5. Trapecio\n");
+		printf("Pulsa cualquier otra tecla para salir.\n");
+		
+			printf("opcion: ");
+				scanf("%d", opcion);
+				
+	switch(opcion) 
+	{
+		case 1:
+			
+			
+		case 2:
+			
+			
+		case 3:
+			
+			
+		case 4:
+			
+			
+		case 5:
+			
+			
+	}			
+				
 	return 0;
 }
