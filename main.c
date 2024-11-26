bc960a537fb2d243400d450563fe0b03cc84bcf8
dani <hectordgames10@gmail.com>
2024-11-26 14:17:01 -0600

    Area de Trapecio

--- main.c
+++ main.c
@@ -3,47 +3,48 @@
 #include <math.h>
 
 int main(int argc, char *argv[]) {
-	
 	int opcion;
 	double lado, resultado;
-do {
-
-
-	printf("Selecciona la figura de la que quieres calcular el area: \n");
+	double baseMenor, baseMayor, altura;
 	
-		printf("1. Circulo\n");
-		printf("2. Triangulo\n");
-		printf("3. Cuadrado\n");
-		printf("4. Rectangulo\n");
-		printf("5. Trapecio\n");
-		printf("introduce cualquier otra cosa para cerrar el programa.\n");
+	do {
+		printf("\nSelecciona la figura de la que quieres calcular el area: \n");
 		
+			printf("1. Circulo\n");
+			printf("2. Triangulo\n");
+			printf("3. Cuadrado\n");
+			printf("4. Rectangulo\n");
+			printf("5. Trapecio\n");
+			printf("\nIntroduce cualquier otra cosa para cerrar el programa.\n");
+			
 			printf("opcion: ");
-				scanf("%d", &opcion);
-				
-	switch(opcion) 
-	{
-		case 1:
-			
-			
-		case 2:
-			
-			
-		case 3:
-				printf("Cuanto mide uno de sus lados? ");
-					scanf("%lf", &lado);
-						resultado = lado * lado;
-				printf("----- Area = %.2lf -----\n", resultado);
-			break;
-			
-		case 4:
-			
-			
-		case 5:
-			
-			
-	}			
-} while (opcion >= 1 && opcion <= 5);
-
+			scanf("%d", &opcion);
+					
+		switch(opcion) 
+		{
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				printf("\nCuanto mide uno de sus lados? ");
+				scanf("%lf", &lado);
+				resultado = lado * lado;
+				printf("\n----- Area = %.2lf -----\n", resultado);
+				break;
+			case 4:
+				break;
+			case 5:
+				printf("\nIngrese base menor: ");
+				scanf("%lf", &baseMenor);
+				printf("Ingrese base mayor: ");
+				scanf("%lf", &baseMayor);
+				printf("Ingrese altura: ");
+				scanf("%lf", &altura);
+				resultado = ((baseMayor + baseMenor) * altura) / 2;
+				printf("\n----- Area = %.2lf -----\n", resultado);
+				break; 
+		}			
+	} while (opcion >= 1 && opcion <= 5);
 	return 0;
 }
