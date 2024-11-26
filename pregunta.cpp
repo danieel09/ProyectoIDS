4b060ab345b1b8976c7fd6910cebdc160eb08e3b
MaxFr300 <a24310119@ceti.mx>
2024-11-26 09:00:23 -0600

    Cuadrado
    Agregue el do, la funcion para calcular el area de cuadrado y corregi errores.

---
+++ pregunta.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	char respuesta[100];
+	
+	printf("Hola como estan, mundo?\n");
+	scanf("%c", &respuesta);
+	
+	printf("ohh muy bien me alegro jajaja");
+	
+return 0;
+}
